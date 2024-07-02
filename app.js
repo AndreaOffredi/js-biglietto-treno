d098295473d3dd5ef28b3ce3f34eba6a2c4c6769
AndreaOffredi <info@andreaoffredi.com>
2024-07-02 19:31:49 +0200

    ADD: Calcolo prezzo considerando lo sconto

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 // Dichiariazione Variabili Input Utente
 let etaUtente
 let kmUtente
-let costoChilometro = 0.21 
+let costoChilometro = 0.21
+let prezzoTotale
+let prezzoSconto
 
 // Assegnazione e Prompt per la Richiesta Input dell'Utente
 kmUtente = prompt('Inserisci il numero di KM') //string | null
+kmUtente = parseInt(kmUtente) //number
 console.log(kmUtente)
 
 etaUtente = prompt('Inserisci la tua età') //string | null
+etaUtente = parseInt(etaUtente) //number
 console.log(etaUtente)
 // 1) Calcolo del Prezzo del Biglietto
 let prezzoBiglietto = kmUtente * costoChilometro
 console.log(prezzoBiglietto) //number
 
-
 // 2) Calcolo dello Sconto del biglietto e del Prezzo Totale
-// -- SE valore età minimo di 18 applica -20%
-// -- SE valoe età supera è uguale o maggiore a 65 applica -40%
-// -- ALTRIMENNTI non applicare nessuno sconto
+if (etaUtente < 18){
+    // -- SE valore età minimo di 18 applica -20%
+    prezzoSconto = prezzoBiglietto * 0.20
+    prezzoTotale = prezzoBiglietto - prezzoSconto
+    console.log(prezzoTotale)
+} else if (etaUtente >= 65){
+    // -- SE valoe età supera è uguale o maggiore a 65 applica -40%
+    prezzoSconto = prezzoBiglietto * 0.40
+    prezzoTotale = prezzoBiglietto - prezzoSconto
+    console.log(prezzoTotale)
+} else {
+    // -- ALTRIMENNTI non applicare nessuno sconto
+    console.log(prezzoTotale)
+}
 // 3) Stampa su HTML
