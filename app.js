20b75e895b4b86ee9c900176ac402a59fece9aa1
AndreaOffredi <info@andreaoffredi.com>
2024-07-02 18:49:50 +0200

    ADD: Description

---
+++ app.js
@@ -0,0 +1,8 @@
+// Dichiariazione Variabili Input Utente
+// Prompt per la Richiesta Input dell'Utente
+// 1) Calcolo del Prezzo del Biglietto
+// 2) Calcolo dello Sconto del biglietto e del Prezzo Totale
+// -- SE valore età minimo di 18 applica -20%
+// -- SE valoe età supera è uguale o maggiore a 65 applica -40%
+// -- ALTRIMENNTI non applicare nessuno sconto
+// 3) Stampa su HTML
