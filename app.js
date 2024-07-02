c54351822d863c7d86097c0749a79b19fe339b8d
AndreaOffredi <info@andreaoffredi.com>
2024-07-02 18:59:37 +0200

    ADD: Calcolo prezzo del bliglietto senza sconto

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 // Dichiariazione Variabili Input Utente
-// Prompt per la Richiesta Input dell'Utente
+let etaUtente
+let kmUtente
+let costoChilometro = 0.21 
+
+// Assegnazione e Prompt per la Richiesta Input dell'Utente
+kmUtente = prompt('Inserisci il numero di KM') //string | null
+console.log(kmUtente)
+
+etaUtente = prompt('Inserisci la tua età') //string | null
+console.log(etaUtente)
 // 1) Calcolo del Prezzo del Biglietto
+let prezzoBiglietto = kmUtente * costoChilometro
+console.log(prezzoBiglietto) //number
+
+
 // 2) Calcolo dello Sconto del biglietto e del Prezzo Totale
 // -- SE valore età minimo di 18 applica -20%
 // -- SE valoe età supera è uguale o maggiore a 65 applica -40%
