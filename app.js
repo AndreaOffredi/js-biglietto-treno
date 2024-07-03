850c98c4efe50993f7c3e81a0e0eec0c0aa4a138
AndreaOffredi <info@andreaoffredi.com>
2024-07-03 17:21:03 +0200

    ADD: modifiche finali

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ let prezzoSconto
 
 // Assegnazione e Prompt per la Richiesta Input dell'Utente
 kmUtente = prompt('Inserisci il numero di KM') //string | null
-kmUtente = parseInt(kmUtente) //number
+kmUtente = parseFloat(kmUtente) //number
 console.log(kmUtente)
 
 etaUtente = prompt('Inserisci la tua età') //string | null
-etaUtente = parseInt(etaUtente) //number
+etaUtente = parseFloat(etaUtente) //number
 console.log(etaUtente)
 
 // 1) Calcolo del Prezzo del Biglietto
@@ -19,12 +19,12 @@ let prezzoBiglietto = kmUtente * costoChilometro
 console.log(prezzoBiglietto) //number
 
 // 2) Calcolo dello Sconto del biglietto e del Prezzo Totale
-if (etaUtente <= 17){
+if (etaUtente < 18){
     // -- SE valore età minimo di 18 applica -20%
     prezzoSconto = prezzoBiglietto * 0.20
     prezzoTotale = prezzoBiglietto - prezzoSconto
     console.log(prezzoTotale)
-} else if (etaUtente >= 65){
+} else if (etaUtente > 65){
     // -- SE valoe età supera è uguale o maggiore a 65 applica -40%
     prezzoSconto = prezzoBiglietto * 0.40
     prezzoTotale = prezzoBiglietto - prezzoSconto
